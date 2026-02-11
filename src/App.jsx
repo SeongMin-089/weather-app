db19de681ce870772191e30f96a271bdf30b8b3d
unknown <a99701841@gmail.com>
2026-02-11 12:26:26 +0900

    bg-img-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import './App.css'
 import WeatherCard from './components/WeatherCard'
-import { useState, useRef, useEffect, useMemo} from 'react'
+import './App.css'
+import { useState, useRef, useEffect,useMemo } from 'react'
 import { fetchCoordinates } from './api/geo'
 import { fetchWeatherByCoords } from './api/weather'
-import { getColorByWeatherId } from './data/bgColor'
-
+import {getColorByWeatherId} from './data/bgColor'
 function App() {
 
-  const [city, setCity]=useState('seoul')
-  const [weather, setWeather]=useState(null)
-  const [loading, setLoading]=useState(false)
-  const [err, setErr]=useState('')
+  const [city, setCity] = useState('seoul')
+  const [weather, setWeather] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState('')
   const inputRef = useRef(null)
 
 
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus()
-  },[])
+  }, [])
+
 
   const bg = useMemo(()=>{
-    const weatherId = weather?.weather?.[0].id
+    const weatherId =weather?.weather?.[0]?.id
 
     return getColorByWeatherId(weatherId ?? 0)
 
   },[weather])
 
-  const handleSearch = async()=>{
+  const handleSearch = async () => {
     const q = city.trim()
-
     if (!q) return
+
     try {
       setLoading(true)
       setErr('')
-      const {lat, lon, name, country} = await fetchCoordinates(q)
+      const { lat, lon, name, country } = await fetchCoordinates(q)
 
       console.log(lat, lon, name, country)
       const data = await fetchWeatherByCoords(lat, lon,)
@@ -46,26 +46,35 @@ function App() {
     } finally {
       setLoading(false)
     }
-  }
 
-  const onChangeInput = (e)=>setCity(e.target.value)
-  const onKeyup = (e)=>{
-    if(e.key==='Enter') handleSearch()
   }
-
+  const onChangeInput = (e) => setCity(e.target.value)
+  const onKeyup = (e) => {
+    if (e.key === 'Enter') handleSearch()
+  }
   return (
-    <div className='app' style={{background:`${bg}`}}>
+    <div className="app" style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      }}>
       <div className="container">
-        <h1>박성민의 날씨앱</h1>
+        <h1>***의 날씨앱</h1>
         <div className="input-wrap">
-          <input value={city} onChange={onChangeInput} onKeyUp={onKeyup} ref={inputRef} type="text" placeholder='도시 이름을 입력하세요'/>
+          <input
+            value={city}
+            onChange={onChangeInput}
+            onKeyUp={onKeyup}
+            ref={inputRef}
+            type="text"
+            placeholder='날씨를 입력하세요' />
           <button
-          onClick={handleSearch}
+            onClick={handleSearch}
           >검색</button>
         </div>
-        {err&&<p>{err}</p>}
-        {loading&&<p>불러오는 중....</p>}
-       <WeatherCard weather={weather}/>
+        {err && <p>{err}</p>}
+        {loading && <p>불러오는 중....</p>}
+        <WeatherCard weather={weather}/>
       </div>
     </div>
   )
